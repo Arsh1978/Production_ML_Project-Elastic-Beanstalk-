4a5e17417f1fe9c7c9b686cad4f1a1ccaba7df59
Arsh <harshtyagi7678@gmail.com>
2023-12-15 23:23:22 +0530

    EDA and Model Training

--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,9 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
+    #the sys.exc_info() has three values in tuple: 1) error type 2) error value 3) traceback object and we are using only the traceback object.
+    #the traceback object has three values in tuple: 1) traceback object 2) line number 3) file name 4) function name
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
     error_message="Error occured in python script name [{0}] line number [{1}] error message[{2}]".format(
@@ -12,18 +14,17 @@ def error_message_detail(error,error_detail:sys):
     
 
 class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
+    def __init__(self,error_message,error_detail:sys):  #here :sys is used as type hint for error_detail
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail=error_detail)
     
     def __str__(self):
         return self.error_message
 
-
-#Testing
-#if __name__ == "__main__":
+# #Testing
+# if __name__ == "__main__":
 #    try:
 #        a=1/0
 #    except Exception as e:
 #        logging.info("Divide by zero error")
-#        raise CustomException(e,sys)
+#        raise CustomException("this is a custom exception",sys)
